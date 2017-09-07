10c8afccec80bb5a821e7a0b7460237f95b9b927
Shade Alabsa <shade34321@gmail.com>
2017-09-06 22:30:57 -0400

    Added in epsilon state.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,6 +6,11 @@ public class main {
         state current_state = state.START;
         int i = 0;
 
+        if (str.length() == 0) { // We received a string of 0 length which fits the epsilon case
+            System.out.println("The string, '', fits the DFA.");
+            return;
+        }
+
         for (char c: s) {
             boolean cero = Character.toString(c).matches("0");
             boolean uno = Character.toString(c).matches("1");
@@ -20,8 +25,8 @@ public class main {
                     i = -1;
                     current_state = state.DEAD;
                 }
-            } else if( uno && (current_state == state.ZERO || current_state == state.ONE)) {
-                if (i >= 1) {
+            } else if( uno && current_state != state.DEAD) {
+                if (i >= 1 || current_state == state.START) {
                     i = 2;
                     current_state = state.ONE;
                     System.out.println("Processing 1");
@@ -39,9 +44,9 @@ public class main {
             }
         }
 
-        if (current_state == state.DEAD || i != 2) {
+        if (current_state == state.DEAD || i < 1) {
             System.out.printf("The string, %s, doesn't match the DFA\n", str);
-        } else if( i == 2) {
+        } else if( i >= 1) {
             System.out.printf("The string, %s, matches the DFA\n", str);
         }
     }
@@ -50,9 +55,15 @@ public class main {
         String test_1 = "0011";
         String test_2 = "10011";
         String test_3 = "001011";
+        String test_4 = "0012";
+        String test_5 = "0000";
+        String test_6 = "1111";
 
         check_state(test_1);
         check_state(test_2);
         check_state(test_3);
+        check_state(test_4);
+        check_state(test_5);
+        check_state(test_6);
     }
 }
